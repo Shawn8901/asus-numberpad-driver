06f2fc72a7615ca1d5eaf31a52360e2d9791299f
Shawn8901 <shawn8901@googlemail.com>
2025-04-20 21:33:44 +0200

    nixos/shell: reformat with nixfmt, remove unused bindings

--- nix/shell.nix
+++ nix/shell.nix
@@ -1,15 +1,12 @@
 {
   python311Packages,
-  pkgs,
   ...
 }:
 let
-  mainPkg = python311Packages.callPackage ./default.nix {};
+  mainPkg = python311Packages.callPackage ./default.nix { };
 in
 mainPkg.overrideAttrs (oa: {
-    nativeBuildInputs =
-      [
-        python311Packages.pip
-      ]
-      ++ (oa.nativeBuildInputs or []);
+  nativeBuildInputs = [
+    python311Packages.pip
+  ] ++ (oa.nativeBuildInputs or [ ]);
 })
